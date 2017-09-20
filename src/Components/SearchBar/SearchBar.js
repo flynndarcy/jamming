e35cd6b8c1324deb9a0d2dade38782699910fff0
Flynn <flynndclark@gmail.com>
2017-09-20 12:05:11 +1000

    NOT VERY FUN

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -8,13 +8,18 @@ class SearchBar extends React.Component
   {
     super(props);
 
+    this.state = {
+
+      term: ''
+    };
+
     this.search = this.search.bind(this);
     this.handleTermChange = this.handleTermChange.bind(this);
   }
 
   search(event)
   {
-    this.props.search(this.state.search);
+    this.props.onSearch(this.state.term);
   }
 
   handleTermChange(event)
