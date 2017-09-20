13cacd007841f4abe8de03cc8298d02f94a07d18
Flynn <flynndclark@gmail.com>
2017-09-20 11:38:18 +1000

    Revision 5

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -9,15 +9,20 @@ class TrackList extends React.Component
   {
     return(
     <div className="TrackList">
-          this.props.tracks.map(track =>  {
-            return <Track key={track.id} track={track} onAdd={this.props.onAdd} onRemove={this.props.onRemove}  />
-          }
-        )
+    this.props.tracks.map(track => {
+      return <Track track={track}
+                    key={track.id}
+                    onAdd={this.props.onAdd}
+                    isRemoval={this.props.isRemoval}
+                    onRemove={this.props.onRemove} />
+    })
 
-    </div>
-  )
+</div>
+);
 
   }
+
+
 
 
 
