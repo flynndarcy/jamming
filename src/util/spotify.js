00b516582d4597f03e393990b7294b9a0a36c165
Flynn <flynndclark@gmail.com>
2017-09-20 11:54:04 +1000

    Updated

--- src/util/spotify.js
+++ src/util/spotify.js
@@ -20,7 +20,7 @@ const spotify = {
       window.history.pushState('Access Token', null, '/'); // This clears the parameters, allowing us to grab a new access token when it expires.
       return accessToken;
     } else {
-      const accessUrl = 'https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}';
+      const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
       window.location = accessUrl;
     }
   },
@@ -66,18 +66,18 @@ return fetch(`https://api.spotify.com/v1/search?type=track&q=term`,
     const accessToken = spotify.getAccessToken();
     const headers = { Authorization: `Bearer ${accessToken}` };
     let userId;
-    return fetch('https://api.spotify.com/v1/me', {headers: headers}
+    return fetch(`https://api.spotify.com/v1/me`, {headers: headers}
     ).then(response => response.json()
     ).then(jsonResponse => {
       userId = jsonResponse.id;
-      return fetch('https://api.spotify.com/v1/users/${userId}/playlists', {
+      return fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, {
         headers: headers,
         method: 'POST',
         body: JSON.stringify({name: name})
       }).then(response => response.json()
       ).then(jsonResponse => {
         const playlistId = jsonResponse.id;
-        return fetch('https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks', {
+        return fetch(`https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks`, {
           headers: headers,
           method: 'POST',
           body: JSON.stringify({uris: uris})
